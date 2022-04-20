c32056a6aaa57785912d3ce39880a2eb8a70a8f6
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-20 17:13:48 -0400

    Add files via upload

---
+++ Program19.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<math.h>
+using namespace std;
+
+int main(){
+    //int x = 5;
+    //int y = 5;
+    //int sum = x + y;
+    
+    string x = "10";
+    string y = "20";
+    string sum = x + y;
+
+    cout << sum << "\n";
+    // It is not possible to merge two different data types i.e., string and int.
+
+    
+    
+    return 0;
+}
