1b02c90fdd5b2e86cf07dfd1cfd4bc5c29f14ae2
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-20 15:50:51 -0400

    Add files via upload

---
+++ Program13.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+  bool isCodingFun = true;          // Boolean Data Type
+  bool isFishTasty = false;
+  cout << isCodingFun << "\n";
+  cout << isFishTasty << "\n" ;
+
+  char myGrade = 'B';     // Character Data Type
+  cout << myGrade << "\n";;
+
+  char a = 65, b = 66, c = 67; // Caital Alphabets
+  cout << a << "\n";;
+  cout << b << "\n";;
+  cout << c << "\n";;
+
+  string greeting = "Hello";  // String data type and declaration
+  cout << greeting << "\n";;
+  return 0;
+}
