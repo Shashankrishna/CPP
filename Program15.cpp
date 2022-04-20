5fcdfcc75a11d07f94f9465695b9cd5a5b978fda
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-20 15:51:19 -0400

    Add files via upload

---
+++ Program15.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 3;
+
+  // Arithematic Operators:
+  
+  cout << x + y << "\n";
+  cout << x - y << "\n";
+  cout << x * y << "\n";
+  cout << x / y << "\n";
+  cout << x % y << "\n";
+  ++x;
+  cout << x << "\n";
+  --y;
+  cout << y << "\n";
+
+  return 0;
+}
