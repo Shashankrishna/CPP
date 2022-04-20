10324f059f94b6151a28781eb02d30d6095f06fe
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-20 17:59:44 -0400

    Add files via upload

---
+++ Program22.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   bool IsCodingFun = false; 
+   bool WillIGetAJob = true;
+   
+  cout<< IsCodingFun << "\n";
+  cout<< WillIGetAJob << "\n";
+  
+  int x = 10;
+  int y = 20;
+  cout << (x > y) << "\n";
+  cout << (x == 15) << "\n";
+  cout << (x < y) << "\n";
+  cout << (x != y) << "\n";
+
+
+    return 0;
+}
