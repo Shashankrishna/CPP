b7a1788664064895a48fc855ee781e0258ccbb22
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-20 15:51:34 -0400

    Add files via upload

---
+++ Program16.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+// Assignment Operators
+
+int main(){
+      int x = 5;
+      cout << x << "\n";
+      
+      x+=3;
+      cout << x << "\n";
+
+      x-=3;
+      cout << x << "\n";
+
+      x*=3;
+      cout << x << "\n";
+
+      x/=3;
+      cout << x << "\n";
+
+      x%=3;
+      cout << x << "\n";
+
+      x&=3;
+      cout << x << "\n";
+
+      x|=3;
+      cout << x << "\n";
+
+      x^=3;
+      cout << x << "\n";
+
+      x>>=3;
+      cout << x << "\n";
+
+      x<<=3;
+      cout << x << "\n";
+
+    return 0;
+}
