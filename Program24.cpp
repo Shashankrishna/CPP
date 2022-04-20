0eeaf0bc2f3fe5cf84f990c203930a957afa8d68
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-20 18:42:23 -0400

    Add files via upload

---
+++ Program24.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int time = 30;
+
+   if(time < 20){
+       cout << "Good Morning" << "\n";
+   }
+   else if (time == 20){
+        cout << "Good afternoon" << "\n";
+   }
+   else {
+       cout << "Good Knight" << "\n";
+   }
+
+    return 0;
+}
