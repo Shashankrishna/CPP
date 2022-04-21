6cff702063a12631c0a4a614c482a024147647f6
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-21 15:48:32 -0400

    Add files via upload

---
+++ Program51.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int myFunction(int x, int y){
+    return (x + y);
+}
+
+int main(){
+    //cout << myFunction(10, 10);
+    //return 0;
+
+    int z = myFunction(10, 10);
+    cout << z; 
+    return 0;
+
+}
+
