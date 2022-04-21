d161e0caec9d0886048422314655b84ef2f4d4da
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-21 12:42:25 -0400

    Add files via upload

---
+++ Program46.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void myFunction(){
+
+    cout << "God Please Help me, I am begging you." << "\n";
+}
+
+int main(){
+    myFunction();
+    myFunction();
+    myFunction();
+    return 0;
+}
+
+// This is a basic function declaration and definition.
