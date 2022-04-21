87c1402c42424755fbd4dbc7163375c6aa3dc0af
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-20 21:46:08 -0400

    Add files via upload

---
+++ Program28.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    //for (i = 0; i < 5; i++){
+     //   cout<< i <<"\n";
+    //}
+
+    for (i = 0; i < 10; i+=2 ){
+        cout << i << "\n";
+    } 
+    return 0;
+}
