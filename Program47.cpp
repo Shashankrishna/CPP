9c07ffdfa49915f841f8b2cd96b8f5c128df937a
Shashankrishna <47184553+Shashankrishna@users.noreply.github.com>
2022-04-21 12:42:44 -0400

    Add files via upload

---
+++ Program47.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+// Function Declaration
+void myFunction();
+
+// main function definition
+int main(){
+
+    myFunction();
+    myFunction();
+    myFunction();
+
+    return 0;
+}
+
+// Function Definition
+void myFunction(){
+    cout << "God, why are you punishing me?" << "\n";
+}
